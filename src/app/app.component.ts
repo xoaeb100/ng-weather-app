f2c2393e186f728b884ca87f37a472f2d736b143
shoaib <shoaib100aib@gmail.com>
2022-05-23 15:36:43 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  cityName: string = 'Alaska';
+  weatherData?: WeatherData;
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = ' ';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = ' ';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      },
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl:
+    'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '9d4225e365msh2d1502e83cb972fp1ff957jsne5621d3126b9',
 };
 
 /*
